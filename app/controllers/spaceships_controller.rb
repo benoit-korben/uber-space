734dc49b770ac2036de93f566b5b90951a03fe29
Benoit GILLIOT <b.gilliot@gmail.com>
2024-05-28 15:23:18 +0200

    add gem faker, add seed, add ruby variable in partial spaceship

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,7 +1,14 @@
 class SpaceshipsController < ApplicationController
   def index
+    @spaceships = Spaceship.all
   end
 
   def show
+  end
+
+  private
+
+  def spaceship_params
+    params.require(:spaceship).permit(:name, :fuel, :number_of_places, :price_per_day, :is_available, :photo)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,34 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "open-uri"
+
+puts "cleaning database"
+Booking.destroy_all
+Spaceship.destroy_all
+User.destroy_all
+
+puts "creating users"
+10.times do
+  User.new(name: Faker::Name.first_name, email: Faker::Internet.email, password: "azerty12345").save
+end
+
+file = URI.open("https://room.eu.com/images/contents/MuSpaceship.jpg")
+10.times do
+  spaceship = Spaceship.new(name: Faker::Lorem.paragraph_by_chars(number: 10),
+  description: Faker::Lorem.paragraph_by_chars(number: 10),
+  number_of_places: rand(0..100),
+  price_per_day: rand(50..5000),
+  fuel: ["Uranium", "Plutonium", "tritium"].sample,
+  is_available: [true, false].sample,
+  user_id: User.all.sample.id,
+  )
+  spaceship.photo.attach(io: file, filename: "spaceship.jpg", content_type: "image/jpg")
+  spaceship.save
+end
+
+10.times do Booking.new(
+  start_date: Faker::Date.between(from: 505.days.ago, to: Date.today),
+  end_date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
+  status: [false, true].sample,
+  spaceship_id: Spaceship.all.sample.id,
+  user_id: User.all.sample.id
+).save
+end
