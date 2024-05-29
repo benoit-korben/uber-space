7415c0e5c0a6a532ca2672dc3e9a414d9474af58
Ismael Perez <ismael.jouhari@gmail.com>
2024-05-29 23:45:39 +0200

    UX redesign + Dashboards controller actions
    - Accordion partial
    - Associated styling
    - Dashboards controller actions enhanced

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,11 +5,12 @@ class DashboardsController < ApplicationController
     if current_user
       @bookings = Booking.where(user_id: current_user.id)
       @pending_bookings = @bookings.where(status: "pending")
-      @confirmed_bookings = @bookings.where(status: "confirmed")
-      @canceled_bookings = @bookings.where(status: "canceled")
+      @confirmed_bookings = @bookings.where(status: "finished")
+      @cancelled_bookings = @bookings.where(status: "cancelled")
+      @rentals = @bookings.where(status: "booked")
+
     end
     @spaceships = current_user ? Spaceship.where(user_id: current_user.id) : []
-    raise
   end
 
 
