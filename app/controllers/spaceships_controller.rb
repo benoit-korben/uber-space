7719efdf8bd8515a49f038a7f278bbe28ffee1c3
Ismael Perez <ismael.jouhari@gmail.com>
2024-05-29 14:17:22 +0200

    Testing Add Spaceship page

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,5 +1,6 @@
 class SpaceshipsController < ApplicationController
   before_action :set_spaceship, only: [:show]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @spaceships = Spaceship.all
@@ -11,15 +12,15 @@ class SpaceshipsController < ApplicationController
     @spaceship = Spaceship.new
   end
 
-  # def create
-  #   @spaceship = Spaceship.new(spaceship_params)
-  #   @spaceship.user = current_user
-  #   if @spaceship.save
-  #     redirect_to spaceship_path(@spaceship)
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @spaceship = Spaceship.new(spaceship_params)
+    @spaceship.user = current_user
+    if @spaceship.save
+      redirect_to spaceship_path(@spaceship)
+    else
+      render :new
+    end
+  end
 
   def set_spaceship
     @spaceship = Spaceship.find(params[:id])
