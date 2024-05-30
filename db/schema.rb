f3960c9d6a4574b03f5f305b5febb82895a8c778
Killian Portier <killian.portier@yahoo.fr>
2024-05-30 18:21:33 +0200

    feat: add favorite model and condition if current user is seller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_30_094826) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_153135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_30_094826) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "spaceship_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["spaceship_id"], name: "index_favorites_on_spaceship_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "spaceships", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -85,5 +94,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_30_094826) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "spaceships"
   add_foreign_key "bookings", "users"
+  add_foreign_key "favorites", "spaceships"
+  add_foreign_key "favorites", "users"
   add_foreign_key "spaceships", "users"
 end
